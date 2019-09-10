9f9dae69d3c8a0c92461a2f728aa861ad02a8a7a
nrunton20 <nrunton20@rthighschool.org>
2019-09-10 16:14:44 -0400

    refined string parse and added __init__

--- output.py
+++ output.py
@@ -5,31 +5,34 @@ output sites after search
 
 output to csv - done
 
-filters through links and emails and exports as csv
+
 '''
 import pandas as pd
 import os
 
 
 class output():
-    
+    '''filters through links and emails and exports as csv'''
+
+    # Initialize data frame with data that has links, emails, and keywords
+    def __init__(self, data):
+        self.df = pd.DataFrame(data)
+
     # Exports data to a csv with the file name: file_name
-    def to_csv(self, file_name, data):
+    def to_csv(self, file_name):
         if file_name[-4:] == '.csv':
-            self._file_name = file_name[0 : (length(file_name) - 4)]
+            self._file_name = file_name[: (len(file_name) - 4)]
         else:
             self._file_name = file_name
         
-        pd.DataFrame(data).to_csv(f'{self._file_name}.csv', index=False)
+        self.df.to_csv(f'{self._file_name}.csv', index=False)
         print(f'Emails and links saved to {os.getcwd()} as {self._file_name}.csv')
 
 
-
-
 if __name__ == '__main__':
-	o = output()
-	o.to_csv('thing', [
-        {'Email(s)': 'stuff', 'Link': 'bla bla', 'Name': 'Duke'},
-        {'Email(s)': 'hellothere', 'Link': 'hahahaha', 'Name': 'Georgia Tech'},
-        {'Email(s)': 'hello there how are you', 'Link': '2e98r232', 'Name': 'NC State'}
+	o = output([
+        {'Email(s)': 'stuff', 'Link': 'bla bla', 'Name': 'Duke', 'Keywords': 'ha, na, ma'},
+        {'Email(s)': 'hellothere', 'Link': 'hahahaha', 'Name': 'Georgia Tech', 'Keywords': 'ha, na, ma'},
+        {'Email(s)': 'hello there how are you', 'Link': '2e98r232', 'Name': 'NC State', 'Keywords': 'ha, na, ma'}
         ])
+	o.to_csv('thing.csv')
