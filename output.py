ae67e5e7967725cd47a86f6fa8b51bf8febd91ae
nrunton20 <nrunton20@rthighschool.org>
2019-09-10 14:54:57 -0400

    added message for export and file download

--- output.py
+++ output.py
@@ -3,18 +3,33 @@ input keywords
 
 output sites after search
 
-output to csv
+output to csv - done
 
+filters through links and emails and exports as csv
 '''
 import pandas as pd
+import os
 
 
-links = [
-    {'Name': 'Duke', 'Link': 'bla bla', 'Email(s)': 'stuff'},
-    {'Name': 'Georgia Tech', 'Link': 'hahahaha', 'Email(s)': 'hellothere'},
-    {'Name': 'NC State', 'Link': '2e98r232', 'Email(s)': 'hello there how are you'}
-]
+class output():
+    
+    # Exports data to a csv with the file name: file_name
+    def to_csv(self, file_name, data):
+        if file_name[-4:] == '.csv':
+            self._file_name = file_name[0 : (length(file_name) - 4)]
+        else:
+            self._file_name = file_name
+        
+        pd.DataFrame(data).to_csv(f'{self._file_name}.csv', index=False)
+        print(f'Emails and links saved to {os.getcwd()} as {self._file_name}.csv')
 
-def to_csv(sites):
-    # sites is a list of dictionaries
-    return pd.DataFrame(sites)
+
+
+
+if __name__ == '__main__':
+	o = output()
+	o.to_csv('thing', [
+        {'Email(s)': 'stuff', 'Link': 'bla bla', 'Name': 'Duke'},
+        {'Email(s)': 'hellothere', 'Link': 'hahahaha', 'Name': 'Georgia Tech'},
+        {'Email(s)': 'hello there how are you', 'Link': '2e98r232', 'Name': 'NC State'}
+        ])
